8789421904c04690630841e2587f160641254cfa
Chetan Solanki <151900157+ChetaN7895@users.noreply.github.com>
2025-03-22 11:37:58 +0530

    Update blog-card.jsx

--- app/components/homepage/blog/blog-card.jsx
+++ app/components/homepage/blog/blog-card.jsx
@@ -1,64 +1,51 @@
 // @flow strict
 import { timeConverter } from '@/utils/time-converter';
-import Image from 'next/image'; 
-import Link from 'next/link'; 
-
+import Image from 'next/image';
+import Link from 'next/link';
 
 
 function BlogCard({ blog }) {
 
   return (
-    <div id='blogs' className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
-      <div className="w-[100px] h-[100px] bg-violet-100 rounded-full absolute top-6 left-[42%] translate-x-1/2 filter blur-3xl  opacity-20"></div>
-
-      <div className="flex justify-center -translate-y-[1px]">
-        <div className="w-3/4">
-          <div className="h-[1px] bg-gradient-to-r from-transparent via-violet-500 to-transparent  w-full" />
+    <div className="border border-[#1d293a] hover:border-[#464c6a] transition-all duration-500 bg-[#1b203e] rounded-lg relative group"
+    >
+      <div className="h-44 lg:h-52 w-auto cursor-pointer overflow-hidden rounded-t-lg">
+        <Image
+          src={blog?.cover_image}
+          height={1080}
+          width={1920}
+          alt=""
+          className='h-full w-full group-hover:scale-110 transition-all duration-300'
+        />
+      </div>
+      <div className="p-2 sm:p-3 flex flex-col">
+        <div className="flex justify-between items-center text-[#16f2b3] text-sm">
+          <p>{timeConverter(blog.published_at)}</p>
+          <div className="flex items-center gap-3">
+            <p className="flex items-center gap-1">
+              <BsHeartFill />
+              <span>{blog.public_reactions_count}</span>
+            </p>
+            {blog.comments_count > 0 &&
+              <p className="flex items-center gap-1">
+                <FaCommentAlt />
+                <span>{blog.comments_count}</span>
+              </p>
+            }
+          </div>
         </div>
-
-      <div className="border border-[#1d293a] hover:border-[#464c6a] transition-all duration-500 bg-[#1b203e] rounded-lg relative group"
-    >
-   <div>
-     <iframe width="560" height="315" src="https://www.youtube.com/embed/RaGS1wuR6GQ?si=vInIX7L3Jcjla1j2" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-    
-</div>
-<div className="border border-[#1d293a] hover:border-[#464c6a] transition-all duration-500 bg-[#1b203e] rounded-lg relative group"
-    >
-<div className="h-44 lg:h-52 w-auto cursor-pointer overflow-hidden rounded-t-lg">
-
-  <div>
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/KcKg5gn0Pqc?si=vSCMmaSiIBVgDJzh" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-</div>
-<div>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/g_OtOgnhSP8?si=i4Dp6oAgD3WWRXtX" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-</div>
-<div>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/FQvQ-_wLIR0?si=GNM_hK4YuO5sJ1Vz" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-</div>
-<div>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/aU1jDsUsgAs?si=BpbfoJy2JMOoq13N" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-</div>
-<div>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/Ql3s3twBDFc?si=hu5K4ApRIeQJ_6z4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-</div>
-<div>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/s8NGt4W_rHQ?si=cCEyXeGx36-GNfzm" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-</div>
-<div>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/d_fXinmskPo?si=dxIallyI9kiwV5gw" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-
-<Link target='_blank' href={blog.url}>
+        <Link target='_blank' href={blog.url}>
           <p className='my-2 lg:my-3 cursor-pointer text-lg text-white sm:text-xl font-medium hover:text-violet-500'>
             {blog.title}
           </p>
-          </Link>
-<p className='text-sm lg:text-base text-[#d3d8e8] pb-3 lg:pb-6 line-clamp-3'>
+        </Link>
+        <p className='mb-2 text-sm text-[#16f2b3]'>
+          {`${blog.reading_time_minutes} Min Read`}
+        </p>
+        <p className='text-sm lg:text-base text-[#d3d8e8] pb-3 lg:pb-6 line-clamp-3'>
           {blog.description}
         </p>
-  </div>       
-  </div>
-  
-   {/* <div className="">
+        {/* <div className="">
           <Link target='_blank' href={blog.url}>
             <button className='bg-violet-500 text-white px-3 py-1.5 rounded-full text-xs'>
               Read More
@@ -66,9 +53,7 @@ function BlogCard({ blog }) {
           </Link>
         </div> */}
       </div>
-      </div>
-      </div>
-      </div>
+    </div>
   );
 };
 
